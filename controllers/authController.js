fd8ada66892b383d654edfa17c784292a869a11c
MatiasChao <matias.chao@despegar.com>
2020-07-08 14:50:52 -0300

    avances

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@
 const { validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const { User } = require('../database')
-const bcrypt = require('bcrypt')
 const bcryptjs = require('bcryptjs')
 
 exports.autenticarUsuario = async (req, res) => {
@@ -13,24 +12,19 @@ exports.autenticarUsuario = async (req, res) => {
         return res.status(400).json({ errors: errors.array() })
     }
 
-    // extraer el email y password
-    const { email, password } = req.body
-
     try {
         // revisar user registrado
         let user = await User.findOne({ where: { email: req.body.email }  })
 
         if(!user) {
-            console.log("no existe el user")
             return res.status(400).json({ msg: 'El usuario no existe' })
         }
 
-        // revisar el password
-        const correctPass = await bcryptjs.compare(password, user.password)
+        // no esta validando el bcrypt ! 
+        const correctPass = await bcryptjs.compare(req.body.password, user.password)
 
-        console.log("correctPass: ", correctPass)
         if(!correctPass) {
-           // return res.status(400).json({ msg: 'Password Incorrecto'})
+           return res.status(400).json({ msg: 'Password Incorrecto'})
         }
 
         //si todo es correcto creamos el JWT
@@ -49,20 +43,8 @@ exports.autenticarUsuario = async (req, res) => {
             // mensaje de confirmacion
             res.send({ token })
         })
-
-
-        console.log("EXISTE EL USUARIO: ", user.password)
-        const iguales = await bcrypt.compareSync(req.body.password, user.password)
-        console.log("Iguales -> ", req.body.password)
-        console.log("Iguales -> ", user.password)
-        // no esta validando el bcrypt ! 
-        if(iguales) {
-            console.log("Login correcto")
-        } else {
-            console.log("Login incorrecto")
-        }
     } catch (error) {
-        console.log(error)
+        res.status(400).json({ msg: 'Algo falló al obtener el usuario'})
     }
 
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,20 @@
 //const User = require("../models/User")
 const jwt = require('jsonwebtoken')
-const bcryptjs = require('bcrypt')
+const bcryptjs = require('bcryptjs')
 const { User } = require('../database')
 require('dotenv').config();
 
 exports.crearUsuario = async (req, res) => {
-
-    // requests lo que el usuario envia
-    console.log(req.body)
-
-    //const { email, password } = req.body
-
     try {
-        //funciona ok
-        //User.create(req.body)
+        const newHash = await bcryptjs.hash(req.body.password, 10)
 
-        //console.log("UNO: " , req.body.password)
-
-        req.body.password = await bcryptjs.hash(req.body.password, 10)
-        //console.log("DOS: " , req.body.password)
-
-        //const user = await User.create(req.body)
-        //res.json(user)
-
-        
         User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: newHash
         })
         
-        
         // Crear y firmar el JWT
-        // intentar pasarle el id del usuario que genero
         const payload = {
             user: {
                 id: req.body.email,
@@ -46,7 +28,6 @@ exports.crearUsuario = async (req, res) => {
         }, (error, token) => {
             if(error) throw error
 
-            // mensaje de confirmacion
             res.send({ token })
         })
 
@@ -54,33 +35,16 @@ exports.crearUsuario = async (req, res) => {
         console.log("Error: ", error)
         res.status(400).send('Hubo un error')
     }
-
-
-    // ESTO GUARDA BN
-    /*
-    User.create({
-        name: 'Matias Chao',
-        email:  'matiaschao22@hotmial.com',
-        password: '12345'
-    })
-    */
 }
 
 exports.getUser = async (req, res) => {
-    console.log('desde getUser', req.user.id)
-
     try {
-        
         const user = await User.findOne({ 
                                 where: { email: req.user.id }, 
                                 attributes: ['id', 'name', 'email']  
                             })
-        console.log("todo ok", user)
-        //res.send(user)
         res.json({user})
-
     } catch (error) {
-        console.log(error)
         res.status(500).json({ msg: 'Hubo un error' })
     }
 }
